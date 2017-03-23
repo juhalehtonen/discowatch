dc869dabaaa03d797788c17884504e1a4b260ddb
Juha Lehtonen <juha.t.lehtonen.91@gmail.com>
2017-03-23 19:04:28 +0200

    Initial commit

---
+++ lib/discowatch.ex
@@ -0,0 +1,18 @@
+defmodule Discowatch do
+  @moduledoc """
+  Documentation for Discowatch.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Discowatch.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/discowatch_test.exs
@@ -0,0 +1,8 @@
+defmodule DiscowatchTest do
+  use ExUnit.Case
+  doctest Discowatch
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
